8648095a0063345c8d34c15a6aab6ea6247eee4e
4vv9int <tannerthayer@gmail.com>
2015-06-10 22:38:37 -0600

    Added spotify app integration to list tracks and url of first album

---
+++ controller.js
@@ -0,0 +1,12 @@
+var songControllers = angular.module('songControllers', []);
+
+songControllers.controller('getAlbumCtrl', ['$scope', '$http',
+        function($scope, $http) {
+ $http.get('http://api.spotify.com/v1/albums/4g1vG8tToNsh49I0fTMY97').success(function(data) {
+            $scope.album = data;
+            $scope.listing = $scope.album.tracks;
+          });
+          
+        }]);
+                            
+
