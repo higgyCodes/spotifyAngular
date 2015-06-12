5b7b6dff9990108593c269f351b514f831cdc718
4vv9int <tannerthayer@gmail.com>
2015-06-12 01:43:00 -0600

    Updated some styles and added play song functionality

--- controller.js
+++ controller.js
@@ -5,7 +5,17 @@ songControllers.controller('getAlbumCtrl', ['$scope', '$http',
  $http.get('http://api.spotify.com/v1/albums/4g1vG8tToNsh49I0fTMY97').success(function(data) {
             $scope.album = data;
             $scope.listing = $scope.album.tracks;
+            $scope.playSong = function(clip) {
+            	
+			 		new Audio($scope.album.tracks.items[clip].preview_url).play();
+            };
+            $scope.pauseSong = function(clip) {
+            	
+			 		new Audio($scope.album.tracks.items[clip].preview_url).pause();
+            };
+
           });
-          
+
         }]);        
+ 
 
