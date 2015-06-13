c49d6aeae6c61a13965b0392ca57d6f94021da41
4vv9int <tannerthayer@gmail.com>
2015-06-12 19:40:59 -0600

    added spotify iframe and disabled sce in order to interpolate over iframe

--- controller.js
+++ controller.js
@@ -1,21 +1,28 @@
-var songControllers = angular.module('songControllers', []);
+var songControllers = angular.module('songControllers', []).config(function($sceProvider) {
+  // Completely disable SCE.  For demonstration purposes only!
+  // Do not use in new projects.
+  $sceProvider.enabled(false);
+});
+
 
 songControllers.controller('getAlbumCtrl', ['$scope', '$http',
         function($scope, $http) {
  $http.get('http://api.spotify.com/v1/albums/4g1vG8tToNsh49I0fTMY97').success(function(data) {
             $scope.album = data;
-            $scope.listing = $scope.album.tracks;
-            $scope.playSong = function(clip) {
-            	
-			 		new Audio($scope.album.tracks.items[clip].preview_url).play();
-            };
-            $scope.pauseSong = function(clip) {
-            	
-			 		new Audio($scope.album.tracks.items[clip].preview_url).pause();
-            };
+            $scope.songArr = ['https://embed.spotify.com/?uri=spotify:track:6iirwBDRrZYduYum9bDlHI', 
+            				  'https://embed.spotify.com/?uri=spotify:track:4QUBEtx8JLogudEkFk4M1R', 
+            				  'https://embed.spotify.com/?uri=spotify:track:32XJDXntXRbGaTW97ckD9P', 
+            				  'https://embed.spotify.com/?uri=spotify:track:7EsoMka4qOVcN8jOh4fv9V', 
+            				  'https://embed.spotify.com/?uri=spotify:track:4nsrGVhD9OQamKiKXCnt9d', 
+            				  'https://embed.spotify.com/?uri=spotify:track:6QxDKRLnQRimykoQKhpuht', 
+            				  'https://embed.spotify.com/?uri=spotify:track:1cYDOpMYxjEN7xLiEqAA5C', 
+            				  'https://embed.spotify.com/?uri=spotify:track:6MqRn6PuVyUeDx8spLwSX4'];
+            console.log($scope.songArr);
+
 
           });
 
         }]);        
  
+
 
